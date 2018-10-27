1d19e2347257044363080103882a9f2b0cc62da9
Andrius Elsbergas <andrius.el@gmail.com>
2018-10-27 07:15:52 +0300

    Create Day16ExeptionsStringToInteger.java

---
+++ java/Day16ExeptionsStringToInteger.java
@@ -0,0 +1,21 @@
+	public static void display(Node head) {
+        Node start = head;
+        while(start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int N = sc.nextInt();
+
+        while(N-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head,ele);
+        }
+        display(head);
+        sc.close();
+    }
+}
