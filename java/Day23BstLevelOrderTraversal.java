1c7f5bc1f39f705a04d31bd156858da56054d554
Andrius Elsbergas <andrius.el@gmail.com>
2018-11-25 07:20:12 +0200

    Update Day23BstLevelOrderTraversal.java

--- java/Day23BstLevelOrderTraversal.java
+++ java/Day23BstLevelOrderTraversal.java
@@ -10,41 +10,42 @@ class Node{
 }
 class Solution{
 
-static void levelOrder(Node root){
-    //Write your code her
-    Queue<Node> queue = new LinkedList<>();
-    queue.add(root);
+    static void levelOrder(Node root){
+        //Write your code her
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
 
-    while (!queue.isEmpty()) {
-        Node removedNode = queue.poll();
-        System.out.print(removedNode.data + " ");
+        while (!queue.isEmpty()) {
+            Node removedNode = queue.poll();
+            System.out.print(removedNode.data + " ");
 
-        if (removedNode.left != null){
-            queue.add(removedNode.left);
-        }
-        if (removedNode.right != null){
-            queue.add(removedNode.right);
-        }
-    }    
-}
-
-public static Node insert(Node root,int data){
-    if(root==null){
-        return new Node(data);
+            if (removedNode.left != null){
+                queue.add(removedNode.left);
+            }
+            if (removedNode.right != null){
+                queue.add(removedNode.right);
+            }
+        }    
     }
-    else{
-        Node cur;
-        if(data<=root.data){
-            cur=insert(root.left,data);
-            root.left=cur;
+
+    public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
         }
         else{
-            cur=insert(root.right,data);
-            root.right=cur;
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
         }
-        return root;
     }
-}
+    
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int T=sc.nextInt();
