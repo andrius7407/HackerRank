76bacebd6c5146c7928d525e7f2078aad247fce1
Andrius Elsbergas <andrius.el@gmail.com>
2018-11-27 21:24:41 +0200

    Update Day24MoreLinkedLists.java

--- java/Day24MoreLinkedLists.java
+++ java/Day24MoreLinkedLists.java
@@ -2,29 +2,29 @@ import java.io.*;
 import java.util.*;
 	
 class Node{
-		int data;
-		Node next;
-		Node(int d){
-			data=d;
-		next=null;
-	}
+	int data;
+	Node next;
+	Node(int d){
+	    data=d;
+	next=null;
+    }
 }
 	
 class Solution{
 		
-	public static Node removeDuplicates(Node head) {
-		//Write your code here
-		if (head == null || head.next == null) {
-		  	return head;
-		}
-		if (head.data == head.next.data) {
-			  head.next = head.next.next;
-			  removeDuplicates(head);
-		} else {
-			removeDuplicates(head.next);
-		}
-		return head;
+    public static Node removeDuplicates(Node head) {
+    //Write your code here
+        if (head == null || head.next == null) {
+	    return head;
 	}
+	if (head.data == head.next.data) {
+	    head.next = head.next.next;
+	    removeDuplicates(head);
+	} else {
+            removeDuplicates(head.next);
+	}
+	return head;
+    }
 
 	public static  Node insert(Node head,int data){
 		Node p=new Node(data);			
