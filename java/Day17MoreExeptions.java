84535af3afcdb6a75460044d17c993fb43de7f05
Andrius Elsbergas <andrius.el@gmail.com>
2018-12-08 22:26:58 +0200

    Update Day17MoreExeptions.java

--- java/Day17MoreExeptions.java
+++ java/Day17MoreExeptions.java
@@ -14,10 +14,10 @@ class Calculator{
 
 class Solution{
 
-    public static void main(String[] args) {
-    
+    public static void main(String[] args) {    
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
+        
         while (t-- > 0) {  
             int n = in.nextInt();
             int p = in.nextInt();
