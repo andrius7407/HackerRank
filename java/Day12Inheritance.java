7f3943f87c93ce2ecf5012dfea0a65edcc1d96fc
Andrius Elsbergas <andrius.el@gmail.com>
2018-12-09 07:13:45 +0200

    Update Day12Inheritance.java

--- java/Day12Inheritance.java
+++ java/Day12Inheritance.java
@@ -1,22 +1,20 @@
 import java.util.*;
 
 class Person {
-	protected String firstName;
-	protected String lastName;
-	protected int idNumber;
+    protected String firstName;
+    protected String lastName;
+    protected int idNumber;
 	
-	// Constructor
-	Person(String firstName, String lastName, int identification){
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.idNumber = identification;
-	}
+    // Constructor
+    Person(String firstName, String lastName, int identification){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = identification;
+    }
 	
-	// Print person data
-	public void printPerson(){
-		 System.out.println(
-				"Name: " + lastName + ", " + firstName 
-			+ 	"\nID: " + idNumber); 
+    // Print person data
+    public void printPerson(){
+        System.out.println("Name: " + lastName + ", " + firstName + "\nID: " + idNumber); 
 	} 
 }
 
